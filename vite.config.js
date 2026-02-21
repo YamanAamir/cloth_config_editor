d7c2dc411ac3825c8edab7aed8e79d7ee5144ade
Yaman Aamir <136349726+YamanAamir@users.noreply.github.com>
2026-02-21 13:29:48 +0500

    added

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import viteImagemin from 'vite-plugin-imagemin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-  base:'/Clothing-Configurator'
-   
-  
+  plugins: [react(), tailwindcss(),
+  viteImagemin({
+    gifsicle: { optimizationLevel: 7 },
+    optipng: { optimizationLevel: 7 },
+    mozjpeg: { quality: 75 },
+    pngquant: { quality: [0.7, 0.9], speed: 4 },
+    svgo: {},
+    webp: { quality: 75 }
+  })
+  ],
+  base: '/Clothing-Configurator'
+
+
 })
